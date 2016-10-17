0e988d449ecefd442414c24f3122eb98c190bb54
dinazoe <dinazouraus@gmail.com>
2016-10-17 03:04:17 -0700

    Doin It
    Simple powershell script ran as scheduled task to Compress and Robocopy
    everything in directory to OneDrive and remove the previous copy.

---
+++ CNB.ps1
@@ -0,0 +1,10 @@
+﻿$Paths = "Path1","Path2","Path3"
+$Date=Get-Date -format M
+$1D = "D:\OneDrive"
+
+Compress-Archive -LiteralPath $paths -CompressionLevel Optimal  -DestinationPath "$1D\$Date"
+if ((gci $1d\*.zip).count -ge "2")
+{
+$old = gci $1d\*.zip | sort LastWriteTime |select -first 1
+rm -Force $old -ErrorAction SilentlyContinue
+}
